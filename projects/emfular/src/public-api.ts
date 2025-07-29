b725ef2bb39ffa31e3a49828cb84581efe16195c
Susanne Göbel <goebel@uni-koblenz.de>
2025-07-29 09:24:30 +0200

    Export ListUpdater

--- projects/emfular/src/public-api.ts
+++ projects/emfular/src/public-api.ts
@@ -6,3 +6,4 @@ export * from './lib/referencing/ref';
 export * from './lib/referencing/referenceable';
 export * from './lib/deserialization/constructor-pointers';
 export * from './lib/deserialization/deserializer';
+export * from './lib/utils/list-updater';
