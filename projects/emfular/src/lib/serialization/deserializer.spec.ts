299955aab48ff942aeabf12ca71a31258a344afa
Susanne Göbel <goebel@uni-koblenz.de>
2026-02-05 15:14:33 +0100

    Make registry implcit, but only as 7.1.0

--- projects/emfular/src/lib/serialization/deserializer.spec.ts
+++ projects/emfular/src/lib/serialization/deserializer.spec.ts
@@ -1,10 +1,7 @@
 import { Deserializer } from './deserializer';
-import {ModelRegistry} from "../binding/model-registry";
 
 describe('Deserializer', () => {
   it('should create an instance', () => {
-    let jsonString = "{}"
-    let json = JSON.parse(jsonString);
-    expect(new Deserializer( new ModelRegistry())).toBeTruthy();
+    expect(new Deserializer()).toBeTruthy();
   });
 });
