8279db0cf019f3e2446a557a53375ea68f333bdd
Susanne Göbel <goebel@uni-koblenz.de>
2026-02-10 16:52:01 +0100

    Working version with all fields optional to allow for emfjackson defaults

--- projects/emfular/src/lib/serialization/json-deserializable.ts
+++ projects/emfular/src/lib/serialization/json-deserializable.ts
@@ -11,17 +11,54 @@ export interface JsonDeserializable<T extends Referencable> {
     new(): T;
 }
 
+// ignore these on attributes:
+type StartsWithPrivate<K> =
+    K extends `_${string}` | `$${string}` ? true : false;
+
+// Identify attribute fields
+type AttributeKeys<T> = {
+    [K in keyof T]:
+    IsContainer<T[K]> extends true ? never :
+        T[K] extends Function ? never :
+            StartsWithPrivate<K> extends true ? never :
+                K
+}[keyof T];
+
+// Remove leading "_" from container field names
+type StripPrivate<K> =
+    K extends `_${infer Rest}` ? Rest : K;
+
+// Identify container fields
+type IsContainer<T> =
+    T extends ReTreeChildrenContainer<any, any> ? true :
+        T extends ReLinkContainer<any, any> ? true :
+            false;
+
+type ContainerKeys<T> = {
+    [K in keyof T]: IsContainer<T[K]> extends true ? K : never
+}[keyof T];
+
+// Map container → JSON
+type JsonForContainer<T> =
+    T extends ReTreeChildrenContainer<infer C, any>
+        ? T extends ReListContainer<any, any>
+            ? JsonOf<C>[]
+            : JsonOf<C> | null
+        : T extends ReLinkContainer<any, any>
+            ? T extends ReListContainer<any, any>
+                ? Ref[]
+                : Ref | null
+            : never;
+
+// Final JSON type
 export type JsonOf<T> =
     & { eClass?: string }
     & {
-        [P in keyof T as T[P] extends Function ? never : P]:
-        T[P] extends ReTreeChildrenContainer<infer C, any>
-         ? T[P] extends ReListContainer<any, any>
-            ? JsonOf<C>[]
-            : JsonOf<C> | null
-        : T[P] extends ReLinkContainer<any, any>
-            ? T[P] extends ReListContainer<any, any>
-                ? Ref[]
-                : Ref | null
-            : T[P];
+    // containers → JSON relationships
+    [K in ContainerKeys<T> as StripPrivate<K>]?:
+    JsonForContainer<T[K]>;
+}
+    & {
+    // attributes → primitive JSON values
+    [K in AttributeKeys<T>]?: T[K];
 };
