4146034c8968a44e6a4cb8b70bd1dec8f5a3e389
Susanne Göbel <goebel@uni-koblenz.de>
2026-02-19 12:10:41 +0100

    Add attributes to metamodel

--- projects/emfular/src/lib/binding/model-definition.ts
+++ projects/emfular/src/lib/binding/model-definition.ts
@@ -1,16 +1,33 @@
 import {Referencable} from "../referencing/referencable/referenceable";
 import {JsonDeserializable} from "../serialization/json-deserializable";
 
+
+export interface EnumDescriptor {
+    name: string;
+    literals: string[];
+}
+
+export interface AttributeDescriptor<T> {
+    name: string;
+    type: T;
+    default?: T | T[];
+
+    /** Cardinality */
+    min?: number;
+    max?: number;
+}
+
+
 export interface FeatureDescriptor<T extends Referencable<any>> {
     name: string;
     type: ClassDescriptor<T>;
     containment: boolean;
     inverse?: string;
+    /** Cardinality */
     min?: number;
     max?: number;
     doc?: string;
 }
-
 
 export interface ClassDescriptor<T extends Referencable<any>> {
     name: string;
@@ -21,12 +38,13 @@ export interface ClassDefinition<T extends Referencable<any>> {
     name: string;
     ctor: JsonDeserializable<T>;
     features: Record<string, FeatureDescriptor<any>>;
+    attributes: Record<string, AttributeDescriptor<any>>
 }
-
 
 export interface ModelDefinition {
     name: string;
     classes: Record<string, ClassDefinition<any>>;
+    enums?: Record<string, EnumDescriptor>;
 }
 
 
