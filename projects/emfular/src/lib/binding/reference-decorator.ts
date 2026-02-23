bf258a431f625c6072239a71f266dd8d398b0d2c
Susanne Göbel <goebel@uni-koblenz.de>
2026-02-23 09:09:19 +0100

    Introduce single interface for precise get type, use it on reference decorator

--- projects/emfular/src/lib/binding/reference-decorator.ts
+++ projects/emfular/src/lib/binding/reference-decorator.ts
@@ -2,6 +2,10 @@ import {Referencable} from "../referencing/referencable/referenceable";
 import {ReferenceMeta} from "./model-definition";
 import {createContainer} from "./reference-creator";
 import {ReContainer} from "../referencing/referencable/container/re-container";
+import {ModelList} from "../referencing/referencable/container/hide/model-list";
+import {createListProxy} from "../referencing/referencable/container/hide/list-proxy";
+import {ReListContainer} from "../referencing/referencable/container/re-list-container";
+import {ReSingleInterface} from "../referencing/referencable/container/re-single-interface";
 
 export function reference<T extends Referencable<any>>(
     meta: ReferenceMeta
@@ -12,20 +16,37 @@ export function reference<T extends Referencable<any>>(
         const symbol = Symbol(String(propertyKey));
         meta.containerKey = symbol;
 
-        Object.defineProperty(prototype, propertyKey, {
-            get(): T | T[] | undefined {
-                const c = this[symbol] as ReContainer<T, any>;
-                return c?.get();
-            },
-            set(value: T | null) {
-                const c = this[symbol] as ReContainer<T, any>;
-                if (!c) throw new Error("Container not initialized");
-                if (value == null) c.delete(); //todo
-                else c.add(value);
-            },
-            enumerable: true,
-            configurable: true
-        });
+        if ( meta.max !== 1) {
+            Object.defineProperty(prototype, propertyKey, {
+                get(): ModelList<T> {
+                    const c = this[symbol] as ReListContainer<T, any>;
+                    return createListProxy(c)
+                },
+                set(_: T | null) {
+                    throw new Error(
+                        `Cannot assign directly to multi-valued reference '${String(propertyKey)}'. Use list operations instead.`
+                    );
+                },
+                enumerable: true,
+                configurable: true
+            });
+        } else {
+            Object.defineProperty(prototype, propertyKey, {
+                get(): T | undefined {
+                    const c = this[symbol] as ReSingleInterface<T, any>;
+                    return c?.get();
+                },
+                set(value: T | null) {
+                    const c = this[symbol] as ReContainer<T, any>;
+                    if (!c) throw new Error("Container not initialized");
+                    if (value == null) c.delete(); //todo
+                    else c.add(value);
+                },
+                enumerable: true,
+                configurable: true
+            });
+
+        }
 
         if (!prototype.__referenceInitializers) {
             prototype.__referenceInitializers = [];
--- projects/emfular/src/lib/referencing/referencable/container/re-single-container.ts
+++ projects/emfular/src/lib/referencing/referencable/container/re-single-container.ts
@@ -1,10 +1,12 @@
 import {Referencable} from "../referenceable";
 import {ReContainer} from "./re-container";
+import {ReSingleInterface} from "./re-single-interface";
 
 export abstract class ReSingleContainer<
     T extends Referencable<any>,
     P extends Referencable<any>
-> extends ReContainer<T, P> {
+> extends ReContainer<T, P>
+implements ReSingleInterface<T, P>{
 
     protected _instance?: T ;
 
