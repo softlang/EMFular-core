fe0740eba1d6a954266e40d9401addd17d8d13ec
Susanne Göbel <goebel@uni-koblenz.de>
2026-02-24 15:05:52 +0100

    Make ModelList fully array compatible by extending array and not omitting methods any more, also remove default for SingleRefs

--- projects/emfular/src/lib/referencing/referencable/container/hide/model-list.ts
+++ projects/emfular/src/lib/referencing/referencable/container/hide/model-list.ts
@@ -1,32 +1,24 @@
-type OmitKeys<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
-
-type ForbiddenArrayMethods =
+export type ForbiddenArrayMethods =
     | "copyWithin"
     | "fill"
     | "sort"
     | "reverse"
-    | "unshift"  // unnecessary and complicated to do
+    | "unshift";
 
 export type RefKind = "tree" | "link" | "parent";
 
-export type SingleRef<
-    T, Kind extends RefKind = "link"
-> = T | undefined;
+export type SingleRef<T, Kind extends RefKind> = T | undefined;
 
-export type ModelList<C> = MetaAwareModelList<C, any>;
+export type ModelList<T> = MetaAwareModelList<T, any>;
 
+export interface MetaAwareModelList<T, Kind extends RefKind>
+    extends Array<T> {
 
-export interface MetaAwareModelList<
-    T, Kind extends RefKind
->
-    extends OmitKeys<Array<T>, ForbiddenArrayMethods> {
-    //additional operations
     move(from: number, to: number): void;
     swap(from: number, to: number): void;
     remove(...items: T[]): boolean;
     delete(): void;
 
-    // internal metadata
     readonly __item: T;
     readonly __kind: Kind;
 }
