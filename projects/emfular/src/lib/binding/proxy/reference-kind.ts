bda6a290eddd872462a177c6849d04185382b724
Susanne Göbel <goebel@uni-koblenz.de>
2026-06-10 13:23:26 +0200

    ALso remove unused KindFromMeta

--- projects/emfular/src/lib/binding/proxy/reference-kind.ts
+++ projects/emfular/src/lib/binding/proxy/reference-kind.ts
@@ -1,7 +1,1 @@
 export type Kind = "tree" | "link" | "none";
-
-export type KindFromMeta<R> =
-    R extends { containment: true } ? "tree"
-        : R extends { isParent: true } ? "none"
-            : R extends { derivingMethod: symbol } ? "none"
-                : "link";
